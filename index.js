afeb832eab9d92471147d1f5f75b12ba69471f15
kaushik-24 <grg23kaushik@gmail.com>
2025-02-25 21:27:24 +0545

    add comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { init } from "./framework";
 import { User } from "./src/user";
 
-const firstName = "Harry";
-const lastName = "Potter";
+const firstName = "";
+const lastName = "";
 
+//instead of declaring variables here
+//you can instead use: init("#app", User({}));
 init("#app", User({ firstName, lastName }));
 
